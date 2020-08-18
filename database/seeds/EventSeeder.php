bea3ca12785f815d389fd4c39a7357c741bfd18a
Nico <nicomilk@gmail.com>
2020-08-18 10:38:36 +0200

    UPDATE event registration info text

--- database/seeds/EventSeeder.php
+++ database/seeds/EventSeeder.php
@@ -29,7 +29,7 @@ class EventSeeder extends Seeder
 
         DB::table('events')->insert
         ([
-            'type'=>'Poste',
+            'type'=>'DPS',
             'description'=>'Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue bottle ennui tbh succulents.',
             'date_start'=>'2020-11-14',
             'date_end'=>'2020-11-15',
